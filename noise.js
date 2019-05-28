b10468f27f6877fd7d73b716dac0e91f34ada601
Kate Sandler <kate@katekatekate.net>
2019-05-28 16:17:36 -0700

    I think it's a piano.

--- noise.js
+++ noise.js
@@ -1,3 +1,19 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(function() {
+  $('.instrument').on('click', 'button', function(event) {
+    $(`#${$(this).html()}Audio`)
+      .get(0)
+      .load();
+    $(`#${$(this).html()}Audio`)
+      .get(0)
+      .play();
+  });
+
+  $('body').keydown(event => {
+    $(`#${event.key}Audio`)
+      .get(0)
+      .load();
+    $(`#${event.key}Audio`)
+      .get(0)
+      .play();
+  });
 });
